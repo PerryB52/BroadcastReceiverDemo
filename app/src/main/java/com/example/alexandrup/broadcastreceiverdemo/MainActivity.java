525e230da6ea5d68a550e712fce07799db7dcf2a
PerryB52 <alex2pereteanu@gmail.com>
2017-01-10 22:33:32 +0200

    commit 10 - dynamic receiver for regular action

--- app/src/main/java/com/example/alexandrup/broadcastreceiverdemo/MainActivity.java
+++ app/src/main/java/com/example/alexandrup/broadcastreceiverdemo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.alexandrup.broadcastreceiverdemo;
 
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -7,10 +8,31 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
+    private MyFirstReceiver myFirstReceiver;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myFirstReceiver = new MyFirstReceiver();
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction("android.intent.action.AIRPLANE_MODE");
+        //intentFilter.addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED);
+
+        registerReceiver(myFirstReceiver, intentFilter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        unregisterReceiver(myFirstReceiver);
+    }
 }
