6e5ac661a9a60bc722455dc8a7d3fae62cf38ea8
PerryB52 <alex2pereteanu@gmail.com>
2017-01-10 22:21:07 +0200

    commit 9 - project setup for dynamic broadcast receiver

--- app/src/main/java/com/example/alexandrup/broadcastreceiverdemo/MainActivity.java
+++ app/src/main/java/com/example/alexandrup/broadcastreceiverdemo/MainActivity.java
@@ -1,14 +1,7 @@
 package com.example.alexandrup.broadcastreceiverdemo;
 
-import android.app.Activity;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,48 +13,4 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void sendBroadcastMessage(View view) {
-
-
-        Intent intent = new Intent("my.custom.action.name");
-
-        //sendBroadcast(intent);
-        //sendOrderedBroadcast(intent, null);
-
-        Bundle b = new Bundle();
-        b.putString("title", "SmartDeveloper");
-
-        sendOrderedBroadcast(intent, null, new MyFourthReceiver(), null, Activity.RESULT_OK, "Android", b);
-    }
-
-    //creating BroadcastReceiver as inner class
-    public static class MyThirdReceiverInner extends BroadcastReceiver {
-
-        private final String TAG = MyThirdReceiverInner.class.getSimpleName();
-
-        @Override
-        public void onReceive(Context context, Intent intent) {
-
-            if (isOrderedBroadcast()) {
-                int initCode = getResultCode();
-                String initData = getResultData();
-                Bundle initBundle = getResultExtras(true);
-                String title = initBundle.getString("title");
-
-                Log.i(TAG, "Code: " + initCode + ", Data: " + initData + ", Bundle: " + title);
-
-                Log.i(TAG, "Hello from 3rd receiver");
-                Toast.makeText(context, "Hello from 3rd Receiver", Toast.LENGTH_LONG).show();
-
-                setResultCode(17);
-                setResultData("iOS");
-
-                initBundle.putString("title", "WiseDeveloper");
-                setResultExtras(initBundle);
-
-            }
-
-
-        }
-    }
 }
--- app/src/main/java/com/example/alexandrup/broadcastreceiverdemo/MyFirstReceiver.java
+++ app/src/main/java/com/example/alexandrup/broadcastreceiverdemo/MyFirstReceiver.java
@@ -3,7 +3,6 @@ package com.example.alexandrup.broadcastreceiverdemo;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -18,24 +17,11 @@ public class MyFirstReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
 
-        if (isOrderedBroadcast()) {
-            int initCode = getResultCode();
-            String initData = getResultData();
-            Bundle initBundle = getResultExtras(true);
-            String title = initBundle.getString("title");
+        Log.i(TAG, "Hello from 1st Receiver, Thread name: " + Thread.currentThread().getName());
 
-            Log.i(TAG, "Code: " + initCode + ", Data: " + initData + ", Bundle: " + title);
-
-
-            Log.i(TAG, "Hello from 1st Receiver, Thread name: " + Thread.currentThread().getName());
-
-            Toast.makeText(context, "Hello from 1st Receiver", Toast.LENGTH_LONG).show();
-
-
-            initBundle.putString("title", "LazyDev");
-            setResult(83, "Windows", initBundle);
-
-        }
+        Toast.makeText(context, "Hello from 1st Receiver", Toast.LENGTH_LONG).show();
 
     }
+
 }
+
