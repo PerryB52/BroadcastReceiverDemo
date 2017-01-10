d80db52b12d3d37d74220f271538ef8ecc49f7b2
PerryB52 <alex2pereteanu@gmail.com>
2017-01-10 22:56:56 +0200

    commit 11 - dynamic receiver for special actions only available for dynamic receivers

--- app/src/main/java/com/example/alexandrup/broadcastreceiverdemo/MainActivity.java
+++ app/src/main/java/com/example/alexandrup/broadcastreceiverdemo/MainActivity.java
@@ -1,19 +1,30 @@
 package com.example.alexandrup.broadcastreceiverdemo;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
     private MyFirstReceiver myFirstReceiver;
+    private TextView textView;
+    private int ctr = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+
 
         myFirstReceiver = new MyFirstReceiver();
     }
@@ -35,4 +46,29 @@ public class MainActivity extends AppCompatActivity {
 
         unregisterReceiver(myFirstReceiver);
     }
+
+    public void registerReceiver(View view) {
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(Intent.ACTION_TIME_TICK);
+
+        registerReceiver(timeTickReceiver,intentFilter);
+
+    }
+
+    public void unregisterTimer(View view) {
+
+        unregisterReceiver(timeTickReceiver);
+    }
+
+    private BroadcastReceiver timeTickReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            int minutes = ctr;
+            textView.setText(minutes + " minute over");
+            ctr++;
+
+            Toast.makeText(context, "Hello from Time Tick Receiver", Toast.LENGTH_SHORT).show();
+        }
+    };
 }
